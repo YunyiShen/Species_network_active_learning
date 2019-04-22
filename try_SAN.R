5c0793e1d067dbb9ff89cfcccb6a856253e4f7f0
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2019-04-22 18:19:14 -0500

    change temp

--- try_SAN.R
+++ try_SAN.R
@@ -1,5 +1,6 @@
 source("SAN.R")
+set.seed(42)
 SANN = run_intermediate_annealing_process(env,already_sampled=Iter_1,n=50,theta=MLE_1$par,nspp=4,
-                                          starting_iteration=100, number_of_iterations=5000,
-                                          s_curve_amplitude=500, s_curve_center=0, 
-                                          s_curve_width=1000,nsample = 300,method = "MH",ncore = 2) 
+                                          starting_iteration=100, number_of_iterations=20000,
+                                          s_curve_amplitude=10, s_curve_center=0, 
+                                          s_curve_width=2500,nsample = 300,method = "MH",ncore = 7) 
