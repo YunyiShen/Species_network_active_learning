941971f547ed8d089c3f763ca64d8bb34040d84b
YunyiShen <yshen99@outlook.com>
2019-04-23 09:57:44 -0500

    minor change

--- try_SAN.R
+++ try_SAN.R
@@ -3,4 +3,4 @@ set.seed(42)
 SANN = run_intermediate_annealing_process(env,already_sampled=Iter_1,n=50,theta=MLE_1$par,nspp=4,
                                           starting_iteration=100, number_of_iterations=5000,
                                           s_curve_amplitude=2, s_curve_center=0, 
-                                          s_curve_width=750,nsample = 300,method = "MH",ncore = 3) 
+                                          s_curve_width=1000,nsample = 300,method = "MH",ncore = 4) 
