86a027a0a6d6d03c8b3d9a4763c8dbfdd7a67c36
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2019-04-24 17:36:43 -0500

    minor change

--- SAN.R
+++ SAN.R
@@ -7,7 +7,7 @@ s_curve = function(x, center, width) {
 } 
 # SANN Modified from Todd. Do data selection based on FI
 
-run_intermediate_annealing_process = function(env_full,already_sampled,n,theta,nspp,
+run_intermediate_annealing_process = function(env_full,already_sampled,n,theta,nspp,nswap = 1,
                                               starting_iteration, number_of_iterations,
                                               s_curve_amplitude, s_curve_center, s_curve_width,nsample = 500,method = "MH",ncore = 4) {
   source("misc.R")
@@ -21,14 +21,15 @@ run_intermediate_annealing_process = function(env_full,already_sampled,n,theta,n
   best_sample = sample_curr
   best_FI = FI_curr
   plot(1,1,type="n",xlim = c(0,number_of_iterations),ylim = c(0,1.5*best_FI))
+  prs = 0
   for(i in 1:number_of_iterations) {
     iter = starting_iteration + i
     temp = current_temperature(iter, s_curve_amplitude, s_curve_center, s_curve_width)
     
     sample_prop = sample_curr
     still_prop = still
-    swap1 = sample(n,1)
-    swap2 = sample(length(still),1)
+    swap1 = sample(n,nswap)
+    swap2 = sample(length(still),nswap)
     
     sample_prop[swap1]=still[swap2]
     still_prop[swap2]=sample_curr[swap1]
@@ -44,15 +45,22 @@ run_intermediate_annealing_process = function(env_full,already_sampled,n,theta,n
       sample_curr = sample_prop
       FI_curr = FI_prop
       still=still_prop
+      prs = 0
       if (FI_curr < best_FI) {
         best_sample = sample_curr
         best_FI = FI_curr
       }
     }
+    else(prs = prs + 1)
+    if(prs==5000){
+      svMisc::progress(100,progress.bar = T)
+      break
+      
+      }
     #if(i%%1==0){cat(i,"\n FI_curr",FI_curr,"\n\n")}
     svMisc::progress(((i-1)/number_of_iterations)*100,progress.bar = T)
     lines(c(i-1,i),c(FI_prev,FI_curr))
   }
-  
+  stopCluster(cl)
   return(list(sample=sample_curr, FI=FI_curr, best_sample=best_sample, best_FI=best_FI))
 }
--- try_SAN.R
+++ try_SAN.R
@@ -1,6 +1,6 @@
 source("SAN.R")
 set.seed(42)
-SANN = run_intermediate_annealing_process(env,already_sampled=Iter_1,n=50,theta=MLE_1$par,nspp=4,
-                                          starting_iteration=100, number_of_iterations=40000,
-                                          s_curve_amplitude=8, s_curve_center=0, 
-                                          s_curve_width=4000,nsample = 300,method = "MH",ncore = 8) 
+SANN = run_intermediate_annealing_process(env,already_sampled=Iter_1,n=50,theta=MLE_1$par,nspp=4,nswap = 3,
+                                          starting_iteration=1, number_of_iterations=15000,
+                                          s_curve_amplitude=4, s_curve_center=0, 
+                                          s_curve_width=2000,nsample = 300,method = "MH",ncore = 8) 
